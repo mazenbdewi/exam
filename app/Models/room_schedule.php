734838f44c7f3bfe450d71221a21627a765ff59b
Mazen <mazen.bdewi@gmail.com>
2025-02-10 12:33:12 +0300

    update room schudulte model

--- app/Models/room_schedule.php
+++ app/Models/room_schedule.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class room_schedule extends Model
 {
     use HasFactory;
+
+    protected $table = 'room_schedules';
+
+    protected $primaryKey = 'room_schedule_id';
+
+    public $timestamps = false;
 }
